0386fd770d6e414e081aa4755194ebffba32ed2e
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2018-09-10 19:45:02 +0200

    environment support

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,13 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map, first, filter } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
-  readonly URL_PATTERN = "http://localhost:8080/guest/{tableId}/state";
 
   private dataObservable: BehaviorSubject<any> = new BehaviorSubject<any>(null);
   private dataAvailableObservable: BehaviorSubject<boolean> = new BehaviorSubject(false);
@@ -18,7 +17,7 @@ export class DataService {
     private http: HttpClient,
     private router: Router,
     private route: ActivatedRoute) {
-    
+
     // subscribe on tableId routeParam
     // based on https://stackoverflow.com/questions/40219790/angular-2-get-routeparams-in-a-service
     this.router.events
@@ -46,21 +45,25 @@ export class DataService {
   private sendRequest(tableId: string) {
     console.log(`subscribing on tableId:${tableId}`);
 
-    const url = this.URL_PATTERN.replace("{tableId}", tableId);
-    const options = {withCredentials: true};
+    const url = `${environment.apiUrl}/guest/${tableId}/state`;
+    const options = { withCredentials: true };
     const httpObservable = this.http.get(url, options).pipe(first());
 
-    httpObservable.subscribe(data => this.dataObservable.next(data));
-    httpObservable.subscribe(() => this.dataAvailableObservable.next(true));
+    httpObservable.subscribe(data => this.onData(data));
+  }
+
+  private onData(data) {
+    this.dataObservable.next(data);
+    this.dataAvailableObservable.next(true)
   }
 
   private rootCategoryNames(data) {
-    if(data == null) {
+    if (data == null) {
       return [];
     }
 
     return data.rootCategories
-      .map(id => this.findCategory(data,id))
+      .map(id => this.findCategory(data, id))
       .filter(category => category != null)
       .map(category => category.name);
   }
