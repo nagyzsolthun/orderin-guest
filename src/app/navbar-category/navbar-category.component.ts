05bb7163ff47605248a7d72bc5a51a7fc2723c84
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2019-03-10 12:10:38 +0100

    implement navbar-category

--- src/app/navbar-category/navbar-category.component.ts
+++ src/app/navbar-category/navbar-category.component.ts
@@ -1,20 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, HostListener, OnDestroy } from '@angular/core';
+import { RouteService } from '../services/route.service';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { first, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar-category',
   templateUrl: './navbar-category.component.html',
   styleUrls: ['./navbar-category.component.css']
 })
-export class NavbarCategoryComponent implements OnInit {
+export class NavbarCategoryComponent implements OnInit, OnDestroy {
 
   @Input() category: string;
+  @HostBinding('class.selected') selected;
+
+  private selectedSubscription: Subscription;
+  private tableId$: Observable<string>;
+
+  constructor(private routeService: RouteService, private router: Router) { }
 
   ngOnInit() {
-    // TODO
+    this.tableId$ = this.routeService.tableId();
+    this.selectedSubscription = this.routeService.category()
+      .pipe(map(category => category == this.category))
+      .subscribe(selected => this.selected = selected);
   }
 
+  ngOnDestroy() {
+    this.selectedSubscription.unsubscribe();
+  }
+
+  @HostListener('click')
   select() {
-    // TODO
+    this.tableId$
+      .pipe(first())
+      .subscribe(tableId => this.router.navigate([tableId, "products", this.category]));
   }
 
 }
