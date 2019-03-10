ce0720adf195d3883dcb98640a7c07bb89d3f0f7
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2019-03-10 12:09:59 +0100

    reorganize RouteService

--- src/app/services/route.service.ts
+++ src/app/services/route.service.ts
@@ -1,34 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Router, ActivatedRoute, NavigationEnd, Params } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd, ParamMap } from '@angular/router';
 
-import { map, first, filter } from 'rxjs/operators';
-import { Observable, BehaviorSubject, ReplaySubject } from 'rxjs';
+import { map, filter, switchMap, shareReplay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 // service to access routeParams outside of the router-outlet
 @Injectable({
   providedIn: 'root'
 })
 export class RouteService {
-
-  private routeParamsObservable = new ReplaySubject<Params>(1);
+  private params$: Observable<ParamMap>;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute) {
 
     // subscribe on routeParams
-    // based on https://stackoverflow.com/questions/40219790/angular-2-get-routeparams-in-a-service
-    this.router.events
+    // based on https://github.com/angular/angular/issues/11023
+    this.params$ = this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
-      .pipe(first())
-      .subscribe(event => this.subscribeOnParams(event));
-  }
-
-  private subscribeOnParams(event) {
-    this.route.firstChild.params.subscribe(params => this.routeParamsObservable.next(params));
+      .pipe(switchMap(() => route.firstChild.paramMap))
+      .pipe(shareReplay(1));
   }
 
   tableId(): Observable<string> {
-    return this.routeParamsObservable.pipe(map(params => params.tableId));
+    return this.params$.pipe(map(params => params.get("tableId")));
+  }
+
+  category(): Observable<string> {
+    return this.params$.pipe(map(params => params.get("category")));
   }
 }
