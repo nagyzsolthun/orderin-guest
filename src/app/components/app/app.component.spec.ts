dfbb7b73d0060aecc088bd8053180ce95b2ccfa8
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2019-08-03 21:18:27 +0200

    remove app title

--- src/app/components/app/app.component.spec.ts
+++ src/app/components/app/app.component.spec.ts
@@ -10,15 +10,11 @@ describe('AppComponent', () => {
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
   
-  it(`should have as title 'orderin-guest'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('orderin-guest');
-  }));
 });
