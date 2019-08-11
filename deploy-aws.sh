ed6ceba038d60cf4f3a48ec637c343a8e892be6c
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2019-08-11 12:02:03 +0200

    rename to orderin-ui-guest

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -4,5 +4,5 @@ if [ $(aws s3 ls orderin-ui-guest/$VERSION/guest/ | wc -l) -gt 0 ]; then
   echo "$VERSION already deployed";
 else
   ng build --prod --configuration production
-  aws s3 cp dist/orderin-guest s3://orderin-ui-guest/$VERSION/guest --recursive
+  aws s3 cp dist/orderin-ui-guest s3://orderin-ui-guest/$VERSION/guest --recursive
 fi
